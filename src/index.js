fc7bd25ee7bc009dfe54a462000ebb939ad2b30b
aliamk <alia.azarian@gmail.com>
2020-05-08 12:10:05 +0100

    removed errorBoundary

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './containers/App'
 import './index.css';
 import 'tachyons';
-import CardList from './components/CardList'
-
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
